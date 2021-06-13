a71a2e503e3218c85c73258d7ebaf68ff5c7afd1
nishthatariyal12 <61908725+nishthatariyal12@users.noreply.github.com>
2021-06-14 01:59:40 +0530

    demo7 using jquery

---
+++ assignment 7/script.js
@@ -0,0 +1,31 @@
+// const languages = new Array();
+// const ul = document.createElement("ul");
+// function onClick(event) {
+//     const language = document.getElementById("language").value;
+//     languages.push(language);
+//     console.log(languages);
+//     updateList(languages.length-1);
+// }
+
+// function updateList(i){
+//     const li = document.createElement("li");// step2
+//     li.innerText = languages[i];//step 3
+//     ul.appendChild(li);//step 4    
+// // // //5.append Ul on index.html
+//     const root=document.getElementById("root");
+//     root.appendChild(ul);
+// }
+
+const languages = new Array();
+const ul = document.createElement("ul");
+$(document).ready(function() {
+  $('#target').click(function() {
+      var content = $('#language').val();
+      languages.push(content)
+      console.log(languages);
+      var fixingContent = 
+          '<li>' + content + '</li>';
+            
+      $('#root').append(fixingContent);
+  })
+})
