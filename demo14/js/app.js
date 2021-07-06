8469e0972cefe8371b0bbb9745455c981c796dc2
nishthatariyal12 <61908725+nishthatariyal12@users.noreply.github.com>
2021-07-06 13:42:39 +0530

    occ 5jul

--- demo14/js/app.js
+++ demo14/js/app.js
@@ -12,12 +12,16 @@ $(document).ready(function(){
     });
    
     function formValidation(){
+        let flag=0;
     const firstName=$("#firstName").val();
     // check for firstName
     // error for firstName
     // check for lastName
     // error messages lname
-    let flag=0;
+
+
+    
+   
     if(firstName==undefined ||firstName.length==0){
         $("#fname_err").text("First Name required!");
         flag=1;
@@ -27,7 +31,21 @@ $(document).ready(function(){
     if(lastName==undefined ||lastName.length==0){
         $("#lname_err").text("Last Name required!");
         flag=1;
-    }   
+    }  
+
+    const Email=$("#Email").val();    
+    if(Email==undefined ||Email.length==0){
+        $("#email_err").text("email required!");
+        flag=1;
+    }  
+    if ($("#gender:selected").length == 0){
+        $("#gender_err").text("gender required!");
+        flag = 1;
+    }
+    if ($("#course:checked").length == 0){
+        $("#course_err").text("course is required");
+        flag = 1;
+        }
 
     if (flag){
         return false
---
+++ occ5jul/js/app.js
@@ -0,0 +1,56 @@
+$(document).ready(function(){
+    $("span").text(" ");
+    
+    $("#submit").click(function(){
+        
+        const isValidated=formValidation();
+        if(isValidated){
+            
+            return true;
+        }else{            
+            return false;
+        }
+    });
+   
+    function formValidation(){
+        let flag=0;
+         if ($("#gender:selected").length == 0){
+        $("#gender_err").text("gender required!");
+        flag = 1;
+    }
+    if ($("#course:checked").length == 0){
+        $("#course_err").text("course is required");
+        flag = 1;
+        }
+
+    if (flag){
+        return false
+    }
+    else{
+        return true
+    }
+   } 
+});
+
+
+
+
+
+function handleFormSubmit(event) {
+  event.preventDefault();
+  
+  const data = new FormData(event.target);
+  
+  const formJSON = Object.fromEntries(data.entries());
+
+  // for multi-selects, we need special handling
+  formJSON.course = data.getAll('course');
+
+//   const results = document.querySelector('.results pre');
+
+//  results.innerText = JSON.stringify(formJSON, null, 2);
+ console.log(JSON.stringify(formJSON));
+}
+
+const form = document.querySelector('.contact-form');
+form.addEventListener('submit', handleFormSubmit);
